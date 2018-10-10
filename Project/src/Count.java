4158d425ac676733640a8198c18b6e1988e19c88
Nagy Krisztina <nagy.krisztina.agnes@gmail.com>
2018-10-10 15:01:59 +0200

    a3 - modified count method

--- Project/src/Count.java
+++ Project/src/Count.java
@@ -1,11 +1,6 @@
 public class Count {
 
-    public static void main(String[] args) {
-        int age = 22;
-        System.out.println(count(age));
-    }
-
-    public String count(int num) {
-        return "My age: " + num;
+    public void count(int monthNumber) {
+        System.out.println(monthNumber);
     }
 }
